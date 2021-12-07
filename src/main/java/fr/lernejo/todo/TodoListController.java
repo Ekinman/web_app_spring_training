da08c970e9d9635fc82a5ed35b15a12de68b3dbd
Olivier AKYOL <olivier.akyol@gmail.com>
2021-12-07 16:12:00 +0100

    Part 4 Filtre pour ajotuer un head HTTP

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,17 +12,17 @@ import java.util.List;
 
 @RestController
 public class TodoListController {
+    private final static List<Todo> todos = new ArrayList<Todo>();
 
-    private static List<Todo> todos = new ArrayList<Todo>();
-
-    @PostMapping(value = "/api/todo")
-    public ResponseEntity<Todo> addTodo(Todo todo){
+    @GetMapping("/api/todo")
+    public ResponseEntity<List<Todo>> getTodos() {
+        return new ResponseEntity<List<Todo>>(todos, HttpStatus.OK);
+    }
+    @PostMapping(value = "/api/todo", consumes = {"application/json"})
+    public ResponseEntity<Todo> addTodo(@RequestBody Todo todo) {
         todos.add(todo);
         return new ResponseEntity<Todo>(todo, HttpStatus.CREATED);
     }
 
-    @GetMapping(value = "/api/todo")
-    public ResponseEntity<List<Todo>> getTodos(){
-        return new ResponseEntity<List<Todo>>(todos, HttpStatus.CREATED);
-    }
+
 }
